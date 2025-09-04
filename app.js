eadde2c37b95f59230554dad4d79d6cb190099b8
Aman <amanfarhan2008@gmail.com>
2025-09-04 21:58:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let para = document.getElementById("special-1").childNodes.parent;
+let div = document.getElementById("div").childNodes[1];
+
+
+
+
+// let sp = para.childNodes;
+
+
+console.log(div);
+
